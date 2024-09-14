8c0bb265210a9785b385a59003f54eb22ba85398
Kyaw Ko Ko Tun <itsbradytun@gmail.com>
2024-09-15 06:05:49 +0630

    added custom donation

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import TicketSubmissionViewSet, PaymentMethodQueryViewSet
+from .views import TicketSubmissionViewSet, PaymentMethodQueryViewSet, CustomDonationViewSet
 
 router = DefaultRouter()
 
 # Register the custom viewsets
-router.register(r'tickets', TicketSubmissionViewSet, basename='ticket-submission')
+router.register(r'tickets', TicketSubmissionViewSet, basename='ticket')
+router.register(r'donate', CustomDonationViewSet, basename='custom-donation')
 router.register(r'payment-methods', PaymentMethodQueryViewSet, basename='paymentmethod-query')
 
 urlpatterns = [
